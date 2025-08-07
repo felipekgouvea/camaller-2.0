825e2b5bfe3c951226699a5b65316c7456776c90
felipekgouvea <felipekinupesg@hotmail.com>
2025-08-07 10:09:19 -0300

    add new products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 import CategorySelector from "@/components/common/category-selector";
 import ProductList from "@/components/common/product-list";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
 
 import { Header } from "../components/common/header";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
     with: {
       variants: true,
     },
@@ -46,6 +55,7 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+        <ProductList products={newlyCreatedProducts} title="Novos Produtos" />
       </div>
     </>
   );
